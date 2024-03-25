aa33085d49eba1673232826aa946c26fe0c84602
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-03-25 18:57:27 +0100

    fallback locale now english

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -3,8 +3,8 @@ import en from "@/locales/en.json";
 import es from "@/locales/es.json";
 
 export const i18n = createI18n({
-  locale: "es",
-  fallbackLocale: "es",
+  locale: "en",
+  fallbackLocale: "en",
   legacy: false,
   messages: {
     en: en,
