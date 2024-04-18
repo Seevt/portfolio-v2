7e6b863af43a817d3ac61fd39342bae11f16fa84
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-04-18 21:46:08 +0200

    work hunt changes

--- src/components/sections/Experience.vue
+++ src/components/sections/Experience.vue
@@ -1,118 +1,126 @@
 <script setup lang="ts">
-import Section from '../Section.vue';
-import { computed } from 'vue'
-import { useI18n } from 'vue-i18n';
+import Section from "../Section.vue";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
 
-const i18n = useI18n()
+const i18n = useI18n();
 
 type Work = {
-    name: string;
-    position: string;
-    remote: boolean;
-    remote_string: string;
-    summary: string;
-    startDate: string;
-    endDate: string;
+  position: string;
+  company: string;
+  remote: boolean;
+  remote_string: string;
+  work_type: string;
+  summary: string;
+  startDate: string;
+  endDate: string;
 };
 
-const work_arr = computed(() => i18n.tm('work') as Work[])
+const work_arr = computed(() => i18n.tm("work") as Work[]);
 </script>
 <template>
-    <Section :title="$t('experience_title')">
-        <ul class="experience-stack">
-            <li v-for="{ name, position, startDate, endDate, summary, remote, remote_string } in work_arr">
-                <article>
-
-                    <div class="position">
-
-                        <h3>
-                            {{ name }} {{ " " }}
-                            <span v-if="remote" class="remote-job">{{ remote_string }}</span>
-                        </h3>
-                        <span aria-role="date">
-                            {{ startDate }}-{{ endDate }}
-                        </span>
-
-                    </div>
-                    <h4>
-                        {{ position }}
-                    </h4>
-                    <p>
-                        {{ summary }}
-                    </p>
-                </article>
-            </li>
-        </ul>
-    </Section>
+  <Section :title="$t('experience_title')">
+    <ul class="experience-stack">
+      <li
+        v-for="{
+          position,
+          company,
+          startDate,
+          endDate,
+          summary,
+          remote,
+          remote_string,
+          work_type,
+        } in work_arr"
+      >
+        <article>
+          <div class="position">
+            <h3>
+              {{ position }} {{ " " }}
+              <span class="remote-job">{{ work_type }}</span>
+              <span v-if="remote" class="remote-job">{{ remote_string }}</span>
+            </h3>
+            <span aria-role="date"> {{ startDate }}-{{ endDate }} </span>
+          </div>
+          <h4>
+            {{ company }}
+          </h4>
+          <p>
+            {{ summary }}
+          </p>
+        </article>
+      </li>
+    </ul>
+  </Section>
 </template>
 <style scoped>
 .experience-stack {
-    --gap: 1rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    margin-top: var(--inner-stack-spacing);
+  --gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-top: var(--inner-stack-spacing);
 }
 
-.experience-stack>* {
-    margin-top: 0;
-    margin-bottom: 0;
+.experience-stack > * {
+  margin-top: 0;
+  margin-bottom: 0;
 }
 
-.experience-stack>*+* {
-    margin-top: var(--gap);
+.experience-stack > * + * {
+  margin-top: var(--gap);
 }
 
 /* article stack */
 article {
-    --gap: 0.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
+  --gap: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 }
 
-article>* {
-    margin-top: 0;
-    margin-bottom: 0;
+article > * {
+  margin-top: 0;
+  margin-bottom: 0;
 }
 
-article>*+* {
-    margin-top: var(--gap);
+article > * + * {
+  margin-top: var(--gap);
 }
 
-article>.position {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+article > .position {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
-.position>h3 {
-    --space: 8px;
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: var(--space);
-    font-size: var(--size-0);
-    font-weight: 600;
-    letter-spacing: 0.009px;
+.position > h3 {
+  --space: 8px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: var(--space);
+  font-size: var(--size-0);
+  font-weight: 600;
+  letter-spacing: 0.009px;
 }
 
-.position>span[aria-role="date"] {
-    font-size: var(--size--2);
-    font-weight: 400;
-    opacity: 0.8;
-    margin-left: 10px;
+.position > span[aria-role="date"] {
+  font-size: var(--size--2);
+  font-weight: 400;
+  opacity: 0.8;
+  margin-left: 10px;
 }
 
-h3>.remote-job {
-    background: hsl(var(--terciary-color) / 0.8);
-    border-radius: calc(var(--border-radius) / 3);
-    padding-block: 2px;
-    padding-inline: 6px;
-    font-size: var(--size--2);
+h3 > .remote-job {
+  background: hsl(var(--terciary-color) / 0.8);
+  border-radius: calc(var(--border-radius) / 3);
+  padding-block: 2px;
+  padding-inline: 6px;
+  font-size: var(--size--2);
 }
 
-article>h4 {
-    font-size: var(--size--2);
+article > h4 {
+  font-size: var(--size--2);
 }
 </style>
