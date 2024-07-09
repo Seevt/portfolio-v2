d61526895be7dc775e2b38910f4faea6c67217a7
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-07-09 21:03:10 +0200

    dimpoint changes

--- src/components/sections/Experience.vue
+++ src/components/sections/Experience.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import Section from "../Section.vue";
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
+  import Section from "../Section.vue";
+  import { computed } from "vue";
+  import { useI18n } from "vue-i18n";
 
-const i18n = useI18n();
+  const i18n = useI18n();
 
-type Work = {
-  position: string;
-  company: string;
-  remote: boolean;
-  remote_string: string;
-  work_type: string;
-  summary: string;
-  startDate: string;
-  endDate: string;
-};
+  type Work = {
+    position: string;
+    company: string;
+    remote: boolean;
+    remote_string: string;
+    work_type: string;
+    summary: string;
+    startDate: string;
+    endDate: string;
+    stillWorking: string;
+  };
 
-const work_arr = computed(() => i18n.tm("work") as Work[]);
+  const work_arr = computed(() => i18n.tm("work") as Work[]);
 </script>
 <template>
   <Section :title="$t('experience_title')">
@@ -31,8 +32,8 @@ const work_arr = computed(() => i18n.tm("work") as Work[]);
           remote,
           remote_string,
           work_type,
-        } in work_arr"
-      >
+          stillWorking,
+        } in work_arr">
         <article>
           <div class="position">
             <h3 class="print">
@@ -44,9 +45,11 @@ const work_arr = computed(() => i18n.tm("work") as Work[]);
               <span class="remote-job">{{ work_type }}</span>
               <span v-if="remote" class="remote-job">{{ remote_string }}</span>
             </h3>
-            <time class="no-print">
+            <time
+              :style="{ fontWeight: stillWorking ? 'bold' : '' }"
+              class="no-print">
               {{ startDate }} -
-              {{ endDate }}
+              {{ !Boolean(stillWorking) ? endDate : stillWorking }}
             </time>
           </div>
           <time datetime="" class="print">
@@ -80,112 +83,112 @@ const work_arr = computed(() => i18n.tm("work") as Work[]);
   </Section>
 </template>
 <style scoped>
-.experience-stack {
-  --gap: 1rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  margin-top: var(--inner-stack-spacing);
-}
+  .experience-stack {
+    --gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    margin-top: var(--inner-stack-spacing);
+  }
 
-.experience-stack > * {
-  margin-top: 0;
-  margin-bottom: 0;
-}
+  .experience-stack > * {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 
-.experience-stack > * + * {
-  margin-top: var(--gap);
-}
+  .experience-stack > * + * {
+    margin-top: var(--gap);
+  }
 
-/* article stack */
-article {
-  --gap: 0.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-}
+  /* article stack */
+  article {
+    --gap: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
 
-article > * {
-  margin-top: 0;
-  margin-bottom: 0;
-}
+  article > * {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 
-article > * + * {
-  margin-top: var(--gap);
-}
+  article > * + * {
+    margin-top: var(--gap);
+  }
 
-article > .position {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
+  article > .position {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 
-.position > h3 {
-  --space: 8px;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: var(--space);
-  font-size: var(--size-0);
-  font-weight: 600;
-  letter-spacing: 0.009px;
-}
+  .position > h3 {
+    --space: 8px;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: var(--space);
+    font-size: var(--size-0);
+    font-weight: 600;
+    letter-spacing: 0.009px;
+  }
 
-.position time {
-  font-size: var(--size--2);
-  font-weight: 400;
-  opacity: 0.8;
-  margin-left: 10px;
-}
+  .position time {
+    font-size: var(--size--2);
+    font-weight: 400;
+    opacity: 0.8;
+    margin-left: 10px;
+  }
 
-h3 > .remote-job {
-  background: hsl(var(--terciary-color) / 0.8);
-  border-radius: calc(var(--border-radius) / 3);
-  padding-block: 2px;
-  padding-inline: 6px;
-  font-size: var(--size--2);
-}
+  h3 > .remote-job {
+    background: hsl(var(--terciary-color) / 0.8);
+    border-radius: calc(var(--border-radius) / 3);
+    padding-block: 2px;
+    padding-inline: 6px;
+    font-size: var(--size--2);
+  }
 
-article > h4 {
-  font-size: var(--size--2);
-}
+  article > h4 {
+    font-size: var(--size--2);
+  }
 
-h3.print {
-  display: none;
-}
-
-time.print {
-  display: none;
-}
-
-@media print {
-  time.no-print {
+  h3.print {
     display: none;
   }
 
   time.print {
-    display: block;
-    font-size: var(--size--2);
-    font-weight: 500;
-  }
-
-  .uppercase {
-    text-transform: capitalize;
-  }
-
-  h3.print {
-    display: block;
-    font-size: var(--size--1);
-    font-weight: 600;
-  }
-
-  .remote-job {
     display: none;
   }
 
-  h3.no-print,
-  h4 {
-    display: none;
+  @media print {
+    time.no-print {
+      display: none;
+    }
+
+    time.print {
+      display: block;
+      font-size: var(--size--2);
+      font-weight: 500;
+    }
+
+    .uppercase {
+      text-transform: capitalize;
+    }
+
+    h3.print {
+      display: block;
+      font-size: var(--size--1);
+      font-weight: 600;
+    }
+
+    .remote-job {
+      display: none;
+    }
+
+    h3.no-print,
+    h4 {
+      display: none;
+    }
   }
-}
 </style>
